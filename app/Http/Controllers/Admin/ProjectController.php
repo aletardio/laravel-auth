a4640c766237551a8e1fe7175179ceace484a47e
aletardio <ale.tardio99@gmail.com>
2024-02-21 17:16:05 +0100

    Created table on index.blade

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -16,9 +16,9 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        $project = Project::all();
+        $projects = Project::all();
 
-        return view('admin.projects.index', compact('project'));
+        return view('admin.projects.index', compact('projects'));
     }
 
     /**
--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -4,7 +4,40 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-                I MIEI PROGETTI
+                <div class="d-flex justify-content-between align-items-center">
+                    <h2>
+                        PROJECTS
+                    </h2>
+                    <div>
+                        <a href="#" class="btn btn-sm btn-primary">Add Project</a>
+                    </div>
+                </div>
+                <div class="col-12">
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Title</th>
+                                <th>Slug</th>
+                                <th>Description</th>
+                                <th>Tools</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($projects as $project)
+                                <tr>
+                                    <td>{{ $project->id }}</td>
+                                    <td>{{ $project->title }}</td>
+                                    <td>{{ $project->slug }}</td>
+                                    <td>{{ Str::limit($project->description, 20, '...') }}</td>
+                                    <td>
+                                        TOOLS
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
